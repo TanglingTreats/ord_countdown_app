e268d80106bf570c493c0e1ce7fbee62ae2515a9
Edwin Tang <edwintang10@yahoo.com.sg>
2019-06-22 21:27:51 +0800

    Restructure application
    Add new file called homepage.dart in ui folder
    
    Move Homepage class from app.dart to homepage.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'ui/homepage.dart';
 
-class MyApp extends StatelessWidget {
+class MyCountdownApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -24,114 +25,8 @@ class MyApp extends StatelessWidget {
         ),
         backgroundColor: Color(0xFF3FB8FF),
       ),
-      home: MyHomePage(title: 'Welcome to Flutter'),
+      home: HomePage(title: 'Welcome to Flutter'),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-  Color _bgColorStart = Color(0xFF3FB8FF); //Blue
-  Color _bgColorEnd = Color(0xFF94D7FF); //Light-white blue
-  
-  int _counter = 0;
-  String actionText = "Default";
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    
-    
-    return Scaffold(
-      body: Stack (children: <Widget>[
-        Container(
-          decoration: BoxDecoration (
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              stops: [0.0, 1.0],
-              colors: [
-                _bgColorStart,
-                _bgColorEnd
-              ]
-            )
-          ),
-        ),
-        new Positioned(
-          top: 0.0,
-          left: 0.0,
-          right: 0.0,
-          child: AppBar(
-            title: Text("ORD Countdown"),
-            centerTitle: true,
-            actions: <Widget>[
-              Container(
-                padding: EdgeInsets.all( 3.5 ),
-                width: 55,
-                child: InkWell(
-                  child: Icon(Icons.menu),
-                  onTap: (){
-                    _incrementCounter();
-                  },
-                  highlightColor: Color(0xFF5291C2),
-                  splashColor: Color(0xFF94D7FF),
-                  borderRadius: BorderRadius.all( Radius.circular(30) ),
-                ),
-              ),
-              SizedBox( width: 20,)
-            ],
-            iconTheme: IconThemeData( size: 30, color: Color(0xFF002D79) ),
-            backgroundColor: Colors.transparent,
-            elevation: 0.0,
-          )
-        ),
-        Center (
-          child: Column (
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-            Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.display1,
-                ),
-            ],
-          )
-        )
-      ]),// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
